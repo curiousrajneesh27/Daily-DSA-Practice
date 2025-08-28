4546e5f650bb4b13a7a950f7542e7eb816291140
Rajneesh verma <curiousrajneesh2024@gmail.com>
2025-08-28 10:04:02 +0530

    Time: 0 ms (100%), Space: 13.4 MB (42.81%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,26 +4,20 @@ using namespace std;
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if (matrix.empty() || matrix[0].empty()) return false;
-        
         int m = matrix.size();
         int n = matrix[0].size();
-        
-        int low = 0, high = m * n - 1;
-        
+
+        int low = 0, high = n * m - 1;
+
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            int midVal = matrix[mid / n][mid % n]; // convert 1D index to 2D
-            
-            if (midVal == target) {
-                return true;
-            } else if (midVal < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+            int row = mid / n;   
+            int col = mid % n;
+
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] < target) low = mid + 1;
+            else high = mid - 1;
         }
-        
         return false;
     }
 };
