eef2c0ca43ec6b54b57d33ac19ba48884e452c0d
Rajneesh verma <curiousrajneesh2024@gmail.com>
2025-09-06 00:26:39 +0530

    Time: 0 ms (100%), Space: 8.9 MB (54.01%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result = "";
+        int open = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                if (open > 0) {  
+                    result += c;
+                }
+                open++;
+            } else {
+                open--;
+                if (open > 0) {
+                    result += c;
+                }
+            }
+        }
+
+        return result;
+    }
+};
