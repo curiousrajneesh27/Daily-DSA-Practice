d5fecd4f6e424bd64f8386b07535715bbd549b15
rajneeshgit <curiousrajneesh2024@gmail.com>
2026-02-13 17:53:41 +0530

    Time: 0 ms (100%), Space: 8.4 MB (96.78%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -2,34 +2,35 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
 
-        if (divisor == 0) return INT_MAX;
-        if (dividend == INT_MIN && divisor == -1) return INT_MAX;
+        if (divisor == 0) return INT_MAX;   // added
+        if (dividend == divisor) return 1;
 
         bool sign = true;
-        if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0))
-            sign = false;
+        if (dividend >= 0 && divisor < 0) sign = false;
+        else if (dividend <= 0 && divisor > 0) sign = false;
 
-        long long n = llabs((long long)dividend);
-        long long d = llabs((long long)divisor);
+        long long n = llabs((long long)dividend);  // fixed
+        long long d = llabs((long long)divisor);   // fixed
 
         long long quotient = 0;
 
         while (n >= d) {
-            long long cnt = 0;
-
+            int cnt = 0;
             while (n >= (d << (cnt + 1))) {
-                cnt++;
+                cnt += 1;
             }
-
-            quotient += (1LL << cnt);
+            quotient += 1LL << cnt;   // fixed
             n -= (d << cnt);
         }
 
-        if (!sign) quotient = -quotient;
+        if (quotient == (1LL << 31) && sign) {   // fixed
+            return INT_MAX;
+        }
 
-        if (quotient > INT_MAX) return INT_MAX;
-        if (quotient < INT_MIN) return INT_MIN;
+        if (quotient == (1LL << 31) && !sign) {  // fixed
+            return INT_MIN;
+        }
 
-        return (int)quotient;
+        return sign ? quotient : -quotient;
     }
 };
